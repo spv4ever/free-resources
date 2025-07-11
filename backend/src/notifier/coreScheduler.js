98194eaa8a1070145e5c29fc4ea1a835de4cd9ab
spv4ever <spv4ever@gmail.com>
2025-07-11 16:27:29 +0200

    Updates cron job schedule to run earlier daily
    Changes the daily event scheduler to trigger at 06:05 instead of 08:50.
    
    This adjustment ensures that event checks occur earlier in the day, potentially improving responsiveness to events.
    
    Logs an additional entry for a job initiation at 14:26 for better tracking.

--- backend/src/notifier/coreScheduler.js
+++ backend/src/notifier/coreScheduler.js
@@ -8,7 +8,7 @@ let dynamicCronTask = null;
 const EVENTS_API_URL = `${process.env.API_URL}/api/sports/events/today`;
 
 export const startDailyEventScheduler = () => {
-  cron.schedule('50 8 * * *', checkIfTodayHasEvents);
+  cron.schedule('5 6 * * *', checkIfTodayHasEvents);
   console.log('📅 Cron diario configurado: revisará eventos a las 00:05 cada día');
 };
 
